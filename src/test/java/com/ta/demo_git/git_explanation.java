cd8c8f44239aed72466377f0a3e5a4e6cf8a7a12
srinidhitekarch <srinidhi.b@tekarch.com>
2020-08-13 23:27:42 +0530

    first commit of my code

---
+++ src/test/java/com/ta/demo_git/git_explanation.java
@@ -0,0 +1,34 @@
+package com.ta.demo_git;
+
+public class git_explanation {
+/*
+ 1. What is repository?
+ place where you can store the project or code
+ 
+ 2. What is GIT?
+ Version control system for tracking changes in source code during software developement
+ 
+ 1 - 1
+ 
+ working on the same project : me , you and deekshith 
+ everyone of us will be keep changing the code --> 2 days once we are releasing witht he latest code
+ per day we are automating 3 test case each --> 9-10 achieve : good to release 2 days once
+ 
+ 10 v0.1 --> 30 v0.2 --> 50 v0.3
+ on top of the previous
+ in a project we can have more than 1 people code can be merged and the same can be released.
+ 
+ 
+ git init --> setup the git to your project
+ you have to specify the repository to whihc you need to push this project
+ 
+git status
+
+git remote add origin <repo url>
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+ 
+ * */
+}
